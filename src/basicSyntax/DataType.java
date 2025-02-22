f7a53ebc22b4dcecfd9479a28065be43005b182c
yutakasakashita0601 <y.sakashita.cingroup2012@gmail.com>
2025-02-22 22:02:27 +0900

    No.9 Java 基礎構文編 2.データ型の課題提出

---
+++ src/basicSyntax/DataType.java
@@ -0,0 +1,13 @@
+package basicSyntax;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		System.out.println("こんにちは" + "私は" + "Geek君です。");
+		System.out.println(10);
+		System.out.println("20" + "24");
+		System.out.println(3.14);
+		System.out.println(true);
+	}
+
+}
