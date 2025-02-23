7706089cf91a3fd6deefcffd26e13fd3c21c5105
yutakasakashita0601 <y.sakashita.cingroup2012@gmail.com>
2025-02-23 10:44:17 +0900

    No.9 Java 基礎構文編 4.四則計算と計算処理の課題提出

---
+++ src/basicSyntax/ComparisonLogicalOperators.java
@@ -0,0 +1,25 @@
+package basicSyntax;
+
+public class ComparisonLogicalOperators {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		boolean isGreatA = (a > b);
+		System.out.println(isGreatA);
+		
+		boolean isSunny = true;
+		boolean isWarm = true;
+		boolean result = isSunny && isWarm;
+		System.out.println(result);
+		
+		int x = 3;
+		int y = 6;
+		boolean isEpual = (x >= 0 && y % 2 == 0);
+		System.out.println(isEpual);
+		
+		boolean hasPermission = false;
+		System.out.println(!hasPermission);
+	}
+
+}
